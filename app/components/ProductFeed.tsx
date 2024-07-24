5aa10527ed7a869c4a98b67b1f20ee28b6ec0c64
Zekaa7 <luka.zekanovic7@gmail.com>
2024-07-24 21:06:56 +0200

    app built

---
+++ app/components/ProductFeed.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import Product from "./Product";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+interface ProductFeedProps {
+  products: Product[];
+}
+
+const ProductFeed: React.FC<ProductFeedProps> = ({ products }) => {
+  return (
+    <div className="grid grid-flow-row-dense md:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4 md:-mt-52">
+      {products
+        .slice(0, 4)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+      <img
+        className="md:col-span-full "
+        src="https://links.papareact.com/dyz"
+        alt=""
+      />
+
+      <div className="md:col-span-2">
+        {products
+          .slice(4, 5)
+          .map(({ id, title, price, description, category, image }) => (
+            <Product
+              key={id}
+              id={id}
+              title={title}
+              price={price}
+              description={description}
+              category={category}
+              image={image}
+            />
+          ))}
+      </div>
+      {products
+        .slice(5, products.length)
+        .map(({ id, title, price, description, category, image }) => (
+          <Product
+            key={id}
+            id={id}
+            title={title}
+            price={price}
+            description={description}
+            category={category}
+            image={image}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default ProductFeed;
